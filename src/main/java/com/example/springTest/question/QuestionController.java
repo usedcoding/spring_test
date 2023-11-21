10879ebd6d03f972d87abac5ca8982645c2ecefd
usedcoding <ja14759@gmail.com>
2023-11-21 13:46:06 +0900

    2-07

--- src/main/java/com/example/springTest/question/QuestionController.java
+++ src/main/java/com/example/springTest/question/QuestionController.java
@@ -1,4 +1,21 @@
 package com.example.springTest.question;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
 public class QuestionController {
+    private final QuestionRepository questionRepository;
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+    //이후 html 코드부터 수정 2-07 '데이터 조회 하여 템플릿에 전달 하기'부터 시작해야 한다.
 }
